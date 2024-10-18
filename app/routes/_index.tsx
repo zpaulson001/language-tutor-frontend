c7af70e2ad073b9d17922fe68ca6a0642467f70b
Zac <zpaulson001@gmail.com>
2024-10-17 21:32:31 -0700

    link to login page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from '@remix-run/node';
-import { Button } from '~/components/ui/button';
+import { NavLink } from '@remix-run/react';
+import { buttonVariants } from '~/components/ui/button';
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,7 +13,9 @@ export default function Index() {
   return (
     <div className="flex flex-col gap-4 h-screen items-center justify-center">
       <h1 className="text-2xl font-bold">Welcome to Language Tutor 🐘</h1>
-      <Button>Get Started</Button>
+      <NavLink to="/login" className={buttonVariants({ variant: 'default' })}>
+        Get Started
+      </NavLink>
     </div>
   );
 }
